b68c404dadc5f531fe05c5013a691e576917c156
ixnp <enprocopios@gmail.com>
2022-10-17 13:22:11 -0700

    solution

---
+++ 04_client_server_communication_II/flatiron_theater_company/client/src/components/Errors.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components"
+
+
+function Errors({errors}){
+   return( <>
+        <GlobalStyle />
+        <h1>Sorry there was an Error</h1>
+        <h1>Please check back later</h1>
+        <h1>🎭{errors}🎭</h1>
+    </>)
+}
+
+export default Errors
+
+const GlobalStyle = createGlobalStyle`
+    background-color:black;
+    color:white;
+`
